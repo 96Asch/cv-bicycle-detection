77090656fa786dd138098ba083cc586cce2f5005
Siert <s.j.g.sebus@student.tudelft.nl>
2022-05-31 13:50:05 +0200

    Finish simple implementation of train_delftbikes.py

--- src/train_delftbikes.py
+++ src/train_delftbikes.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import argparse
-import numpy as np
+import tensorflow as tf
+from pycocotools.coco import COCO
+
 from mrcnn.config import Config
 from mrcnn import utils
 import mrcnn.model as modellib
-from mrcnn import visualize
-from mrcnn.model import log
+from samples.coco.coco import CocoDataset, DEFAULT_DATASET_YEAR
 
 
 # Root directory of the project
@@ -35,23 +36,79 @@ class DelftBikesConfig(Config):
     TRAIN_ROIS_PER_IMAGE = 64
 
 
-class DelftBikesDataSet(utils.Dataset):
-    pass
+class DelftBikesDataSet(CocoDataset):
+    def load_delft_bikes(self, dataset_dir: str, subset: str):
+        coco = COCO("{}/annotations/instances_{}{}.json".format(dataset_dir, subset,
+                                                                DEFAULT_DATASET_YEAR))
+        class_ids = coco.getCatIds(catNms=['BG', 'bicycle'])
+        self.load_coco(dataset_dir, subset, class_ids=class_ids)
+
+        # assert subset in ['all', 'train', 'val']
+        #
+        # coco = COCO("{}/coco_extended_with_delftbikes_{}.json".format(dataset_dir, subset))
+        # image_dir = "{}/{}".format(dataset_dir, subset)
+        # class_ids = coco.getCatIds(catNms=['BG', 'bicycle'])
+        #
+        # # Add classes
+        # for i in class_ids:
+        #     self.add_class("coco", i, coco.loadCats(i)[0]["name"])
+        #
+        # image_ids = list(coco.imgs.keys())
+        #
+        # # Add images
+        # for i in image_ids:
+        #     self.add_image(
+        #         "coco", image_id=i,
+        #         path=os.path.join(image_dir, coco.imgs[i]['file_name']),
+        #         width=coco.imgs[i]["width"],
+        #         height=coco.imgs[i]["height"],
+        #         annotations=coco.loadAnns(coco.getAnnIds(
+        #             imgIds=[i], catIds=class_ids, iscrowd=None)))
 
 
 def setup_parse():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--model', required=True, type=str, help='Path to trained weights')
-    # parser.add_argument('--input', required=True, type=str, help='Path to dataset')
-    # parser.add_argument('--exps', required=True, type=str, help='Path to store trained model/logs')
+    parser.add_argument('--model', required=True, type=str, help='Path to trained weights')
+    parser.add_argument('--input', required=True, type=str, help='Path to dataset')
+    parser.add_argument('--exps', required=True, type=str, help='Path to store trained model/logs')
+    parser.add_argument('--mode', required=True, type=str,
+                        help='Either "training" or "inference" mode')
+    parser.add_argument('--device', required=False, type=str, default='/gpu:0',
+                        help='The device to run on: either /cpu:0 or /gpu:0')
+    parser.add_argument('--layers', )
     return parser.parse_args()
 
 
-def main(argv):
+def main(args):
     config = DelftBikesConfig()
 
+    model_save_dir = os.path.abspath(args.exps)
+    mode = os.path.abspath(args.mode)
+    pretrained_model_file = os.path.abspath(args.model)
+    data_set_dir = os.path.abspath(args.input)
+
+    assert mode in ['training', 'inference']
+
+    with tf.device(args.device):
+        model = modellib.MaskRCNN(mode=mode, config=config, model_dir=model_save_dir)
+
+    model.load_weights(pretrained_model_file)
+
+    if model == 'training':
+        dataset_train = DelftBikesDataSet()
+        dataset_train.load_delft_bikes(data_set_dir, 'train')
+        dataset_val = DelftBikesDataSet()
+        dataset_train.load_delft_bikes(data_set_dir, 'val')
+
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=1,
+                    layers='heads')
+
+    elif model == 'inference':
+        pass
 
 
 if __name__ == '__main__':
-    argv = setup_parse()
-    main(argv)
+    args = setup_parse()
+    main(args)
